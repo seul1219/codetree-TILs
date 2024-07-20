bb35a10480117ead947c62cba16c67e77a7050ca
seul1219 <125745601+seul1219@users.noreply.github.com>
2024-07-21 07:40:21 +0900

    Title: 행복한 수열의 개수, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240721/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] numbers = new int[N][N];
+        for(int r=0; r<N; r++) {
+            st = new StringTokenizer(br.readLine());
+            for(int c=0; c<N; c++) {
+                numbers[r][c] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int ans = 0;
+
+        //행 탐색
+        for(int r=0; r<N; r++) {
+            int com = numbers[r][0];
+            int cnt = 1;
+
+            for(int c=1; c<N; c++) {
+                if(numbers[r][c]==com) {
+                    cnt++;
+
+                    if(cnt>=M) {
+                        ans++;
+                        break;
+                    }
+                } else {
+                    com = numbers[r][c];
+                    cnt=1;
+                }
+            }
+        }
+        
+        //열 탐색
+        for(int c=0; c<N; c++) {
+            int com = numbers[0][c];
+            int cnt = 1;
+
+            for(int r=1; r<N; r++) {
+                if(numbers[r][c]==com) {
+                    cnt++;
+
+                    if(cnt>=M) {
+                        ans++;
+                        break;
+                    }
+                } else {
+                    com = numbers[r][c];
+                    cnt=1;
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
