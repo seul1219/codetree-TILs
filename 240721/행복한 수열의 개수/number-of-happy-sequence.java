8e54e38585a2c008723faab136d557b2bfb029e9
seul1219 <125745601+seul1219@users.noreply.github.com>
2024-07-21 07:42:20 +0900

    Title: 행복한 수열의 개수, Time: 110ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240721/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240721/행복한 수열의 개수/number-of-happy-sequence.java
@@ -17,48 +17,52 @@ public class Main {
             }
         }
 
-        int ans = 0;
+        if(M==1) {
+            System.out.println(N*N);
+        } else {
+            int ans = 0;
 
-        //행 탐색
-        for(int r=0; r<N; r++) {
-            int com = numbers[r][0];
-            int cnt = 1;
+            //행 탐색
+            for(int r=0; r<N; r++) {
+                int com = numbers[r][0];
+                int cnt = 1;
 
-            for(int c=1; c<N; c++) {
-                if(numbers[r][c]==com) {
-                    cnt++;
+                for(int c=1; c<N; c++) {
+                    if(numbers[r][c]==com) {
+                        cnt++;
 
-                    if(cnt>=M) {
-                        ans++;
-                        break;
+                        if(cnt>=M) {
+                            ans++;
+                            break;
+                        }
+                    } else {
+                        com = numbers[r][c];
+                        cnt=1;
                     }
-                } else {
-                    com = numbers[r][c];
-                    cnt=1;
                 }
             }
-        }
-        
-        //열 탐색
-        for(int c=0; c<N; c++) {
-            int com = numbers[0][c];
-            int cnt = 1;
+            
+            //열 탐색
+            for(int c=0; c<N; c++) {
+                int com = numbers[0][c];
+                int cnt = 1;
 
-            for(int r=1; r<N; r++) {
-                if(numbers[r][c]==com) {
-                    cnt++;
+                for(int r=1; r<N; r++) {
+                    if(numbers[r][c]==com) {
+                        cnt++;
 
-                    if(cnt>=M) {
-                        ans++;
-                        break;
+                        if(cnt>=M) {
+                            ans++;
+                            break;
+                        }
+                    } else {
+                        com = numbers[r][c];
+                        cnt=1;
                     }
-                } else {
-                    com = numbers[r][c];
-                    cnt=1;
                 }
             }
-        }
 
-        System.out.println(ans);
+            System.out.println(ans);
+        }
     }
 }
