0a54439186c55fb372524c15f10f5621be9ef429
seul1219 <125745601+seul1219@users.noreply.github.com>
2024-08-18 01:58:19 +0900

    Title: 뿌요뿌요, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240818/뿌요뿌요/puyo-puyo.java
+++ 240818/뿌요뿌요/puyo-puyo.java
@@ -5,7 +5,7 @@ public class Main {
     public static int N;
     public static int[][] num;
     public static boolean[][] visited;
-    public static int max = 0;
+    public static int max;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
