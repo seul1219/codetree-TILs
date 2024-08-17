5c1eb704337fce5f35f5558a7f51c1d2868a487b
seul1219 <125745601+seul1219@users.noreply.github.com>
2024-08-18 00:53:36 +0900

    Title: 그래프 탐색, Time: 207ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240818/그래프 탐색/graph-traversal.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int N;
+    public static int M;
+    public static List<Integer>[] list;
+    public static boolean[] visited;
+    public static int res = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        list = new LinkedList[N+1];
+        for(int i=0; i<N+1; i++) {
+            list[i] = new LinkedList<>();
+        }
+
+        visited = new boolean[N+1];
+
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            list[start].add(end);
+            list[end].add(start);
+        }
+
+        visited[1] = true;
+        search(1);
+
+        System.out.println(res);
+    }
+
+    public static void search(int num) {
+        for(int i=0; i<list[num].size(); i++) {
+            int cur = list[num].get(i);
+            if(!visited[cur]) {
+                visited[cur] = true;
+                res++;
+                search(cur);
+            }
+        }
+    }
+}
