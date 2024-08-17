82745f648f19f4d5eb3808acc35ebfcaf3b5bfab
seul1219 <125745601+seul1219@users.noreply.github.com>
2024-08-17 22:49:47 +0900

    Title: 크기가 n인 순열, Time: 175ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 240817/크기가 n인 순열/n-permutation.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int N;
+    public static int[] arr;
+    public static boolean[] visited;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+        visited = new boolean[N+1];
+
+        select(0);
+        System.out.println(sb.toString());
+    }
+
+    public static void select(int idx) {
+        if(idx==N) {
+            for(int i=0; i<N; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        for(int i=1; i<=N; i++) {
+            if(!visited[i]) {
+                arr[idx] = i;
+                visited[i] = true;
+                select(idx+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
